0f715dd92008eace72a6c3887ef296a01916d477
Mihir Bafna <mihir.bafna@gmail.com>
2019-01-07 22:29:27 -0800

    minor edits

--- Weapon.java
+++ Weapon.java
@@ -7,22 +7,17 @@ public class Weapon extends GameObject{
 	private double orthoY;
 	private double newDeltaX;
 	private double newDeltaY;
-	private double oX;
-	private double cX;
-	private double oY;
-	private double cY;
 	private double deltaY;
 	private double deltaX;
 	private double newLength;
 	private GameObject attached;
 	private double aX;
 	private double aY;
-	private double angle=0;
 	private double d= 0;
 	
 	public Weapon(ImageIcon img, int x, int y, int size, String key, GameObject attached) {
 		super(img, x, y, size, key);
-		this.setAttached(attached);
+		this.attached = attached;
 	}
 
 	
@@ -40,47 +35,15 @@ public class Weapon extends GameObject{
 		aX = attached.getXPos()+newDeltaX*radius/newLength; aY = attached.getYPos()+newDeltaY*radius/newLength;
 		setXPos((int) aX);
 		setYPos((int) aY);
-		System.out.println("x = " + aX);
-		System.out.println("y = " + aY);
-		
-		
-		/*angle = .8;
-		angle = (angle ) * (Math.PI/180); // Convert to radians
-		double pointx = getXPos();
-		double pointy = getYPos();
-		double centerx = attached.getXPos() + attached.getSize()/2;
-		double centery = attached.getYPos() + attached.getSize()/2;
-        double rotatedX = Math.cos(angle) * (pointx - centerx) - Math.sin(angle) * (pointy-centery) + centerx;
-        double rotatedY = Math.sin(angle) * (pointx - centerx) + Math.cos(angle) * (pointy - centery) + centery;
-        setXPos((int)rotatedX);
-        setYPos((int) rotatedY);
-        */
 
 	}
 
-
-	@Override
 	public void events() {
-		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void init() {
 		gameObjects.put(id, this);
-		
-	}
-
-
-
-	public GameObject getAttached() {
-		return attached;
-	}
-
-
-
-	public void setAttached(GameObject attached) {
-		this.attached = attached;
 	}
 
 
